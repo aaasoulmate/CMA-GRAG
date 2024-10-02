9e4f2754b0362a4c6afea21b153c16b5e557ed89
aaasoulmate <104032338+aaasoulmate@users.noreply.github.com>
2024-10-02 13:45:20 +0800

    Update chat.py

--- utils/chat.py
+++ utils/chat.py
@@ -1,7 +1,9 @@
 """
 Here, we consider the family of GLM-4 Large Langugae Models as our pre-trained baseline chat models.
 The provided LLMs here are:
-
+glm_4_list = ["glm-4", "glm-4-plus", "glm-4-long", "glm-4-0520", "glm-4-flash"]
+My api_key for GLM is not provided here; which you can access to one by
+https://www.bigmodel.cn/usercenter/apikeys
 """
 
 import json
